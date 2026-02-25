5e738904b7421385b934c6669ef8c3014d7d3e64
lucaserre <lucasrabeloalmeida@hotmail.com>
2026-02-24 21:33:54 -0300

    Terceiro dia de imersão py

---
+++ Dia_03/dia_3.py
@@ -0,0 +1,12 @@
+#criar um dicionário
+#alimentar com informações como: cliente, parcelas_atraso_vip e telefones
+
+contrato = {
+    'cliente' : 'Lucas',
+    'parcelas_atraso' : 2,
+    'vip' : True,
+    'telefones' : ['3199999-8888', '31 98888-9999']
+
+}
+
+print(contrato['telefones'][1])
