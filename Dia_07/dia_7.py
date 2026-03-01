30cc88e7e44b6aea0c5f0f6841b5c64aca4d241b
Lucas R. <lucasrabeloalmeida@hotmail.com>
2026-02-28 22:00:55 -0300

    Rename function and update print statement

--- Dia_07/dia_7.py
+++ Dia_07/dia_7.py
@@ -1,8 +1,7 @@
 
-def calcular_multa_errada(valor_original):
+def calcular_multa(valor_original):
     valor_atualizado = valor_original + 2
-    print(f"O valor lá dentro é {valor_atualizado}")
+    return valor_atualizado
 
-boleto = calcular_multa_errada(100)
-
-print(f"O valor que eu peguei na mão foi: {boleto}")
+boleto = calcular_multa(100)
+print(boleto)
